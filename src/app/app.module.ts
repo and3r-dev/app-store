4c02b8e1e06b2f142dc13fc06d59f7b0a8e37051
and3r-dev <neo.and3r@gmail.com>
2024-07-05 15:46:38 -0300

    ações no carrinho de compras, implementação de store com manipulação de eventos, view itens

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,21 +7,32 @@ import { StartComponent } from './pages/start/start.component';
 import { NavbarComponent } from './layouts/navbar/navbar.component';
 import { HttpClientModule } from '@angular/common/http';
 import { ProductComponent } from './pages/product/product.component';
+import { StoreShoppingCart } from './store/shopping-cart.store';
+import { ItensComponent } from './pages/itens/itens.component';
+
+import { SweetAlert2Module } from '@sweetalert2/ngx-sweetalert2';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ToastrModule } from 'ngx-toastr';
 
 @NgModule({
   declarations: [
     AppComponent,
     StartComponent,
     NavbarComponent,
-    ProductComponent
+    ProductComponent,
+    ItensComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
+    SweetAlert2Module.forRoot(),
+    BrowserAnimationsModule,
+    ToastrModule.forRoot(),
   ],
   providers: [
-    provideClientHydration()
+    provideClientHydration(),
+    StoreShoppingCart
   ],
   bootstrap: [AppComponent]
 })
--- src/app/pages/product/product.component.ts
+++ src/app/pages/product/product.component.ts
@@ -1,5 +1,7 @@
 import { Component, Input } from '@angular/core';
 import { ProductsService } from '../../services/products/products.service';
+import { BuyTable } from '../../interfaces/buy-table';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-product',
@@ -7,20 +9,56 @@ import { ProductsService } from '../../services/products/products.service';
   styleUrl: './product.component.css'
 })
 export class ProductComponent {
+  productsCart: BuyTable[] = [];
+  countProducts: number = 0;
 
   @Input() product: any;
 
   constructor(
-    private _productsService: ProductsService
-  ) {}
+    private _productsService: ProductsService,
+    private toastr: ToastrService
+  ) { }
 
   adicionar() {
-    console.log(this.product);
-    this._productsService.postProducts(this.product).subscribe({
-      // next: x => console.log('The next value is: ', x),
-      error: err => console.error('Ocorreu erro :', err),
-      complete: () => console.log('finalizado com sucesso.')
+    let updatedItem = false;
+    this.productsCart.forEach((item) => {
+      if (item.id === this.product.id && !updatedItem) {
+        item.amount += 1;
+        item.subtotal = item.amount * item.price;
+        updatedItem = true;
+        this.putShoppingCart(item);
+        return;
+      }
+    });
+
+    if (!updatedItem)
+      this.postShoppingCart();
+  }
+
+  getProductsCart() {
+    this._productsService.getProductsCart().subscribe((response) => {
+      this.productsCart = response;
+      this.countProducts = this.productsCart.length;
     });
   }
 
+  postShoppingCart() {
+    this.product.amount = 1;
+    this.product.subtotal = parseInt(this.product.price);
+    this._productsService.postProducts(this.product).subscribe({
+      error: err => console.error('Ocorreu erro :', err),
+      complete: () => {
+        this._productsService.setCart(this.product);
+        this.productsCart.push(this.product);
+        this.countProducts++;
+        this.toastr.success('Adicionado ao carrinho!', 'Novo Item.');
+      }
+    });
+  }
+
+  putShoppingCart(item: BuyTable) {
+    this._productsService.putProducts(item.id, item).subscribe((response) => {
+      this.toastr.success('Adicionado ao carrinho!', `Item com ${item.amount} unidades.`);
+    });
+  }
 }
